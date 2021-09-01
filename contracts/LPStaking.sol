88334eb6056193e8cd9ea1a994f69bb3d24fa51c
wanglonghong <wanglonghong4467@gmail.com>
2021-09-01 17:21:36 +0800

    Update LPStaking.sol

--- contracts/LPStaking.sol
+++ contracts/LPStaking.sol
@@ -15,137 +15,176 @@ contract LPStaking is ReentrancyGuard, Pausable, Ownable {
 
     uint256 constant public PERCENTS_DIVIDER = 1000;
 
-    /* LP Token */
-    address public LPAddress;
-    /* Reward Token */
-    address public rewardTokenAddress;
+    struct PoolInfo {
+        /* LP Token */
+        address LPAddress;
+        /* Reward Token */
+        address rewardTokenAddress;
 
-    /* Staked LP token balances*/
-    mapping(address => uint256) public balances;
-    /* Reward balances */
-    mapping(address => uint256) public rewards;
-    /* Last updated time */
-    mapping(address => uint256) public lastUpdated;
+        /* Staking Reward ratio ( every 1 week ) */
+        uint256 rewardPerWeek;
 
-    /* How many tokens we have successfully staked */
-    uint256 public totalStaked;
+        uint256 minStakingAmount;
+        uint256 maxStakingAmount;
 
-    /* Staking Reward ratio ( every 1 week ) */
-    uint256 public rewardPerWeek;
+        /* Penelty Fee for unstaking (%) */
+        uint unstakingFee;
+            
+        /* How many tokens we have successfully staked */
+        uint256 totalStaked;
+    }
+    
+    struct UserInfo {
+        uint256 balance;
+        uint256 rewards;
+        uint256 lastUpdated;
+    }
 
-    uint256 public minStakingAmount;
-    uint256 public maxStakingAmount;
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
 
-    /* Penelty Fee for unstaking (%) */
-    uint public unstakingFee;
+    mapping(address => bool) public poolExistence;
+
     /* Address for collecting fee */
     address public feeAddress;
 
-    event RewardAddressUpdated(address token);
-    event RewardPerWeekUpdated(uint256 reward);
+    event RewardAddressUpdated(uint256 pid, address token);
+    event RewardPerWeekUpdated(uint256 pid, uint256 reward);
 
-    event Staked(address indexed account, uint256 amount);
-    event Withdrawn(address indexed account, uint256 amount);
-    event Redeem(address indexed account, uint256 amount);
+    event Staked(uint256 pid, address indexed account, uint256 amount);
+    event Withdrawn(uint256 pid, address indexed account, uint256 amount);
+    event Redeem(uint256 pid, address indexed account, uint256 amount);
     
 
-    modifier updateReward(address account) {
+    modifier updateReward(uint256 pid, address account) {
         if (account != address(0)) {
-            rewards[account] = earned(account);
-            lastUpdated[account] = block.timestamp;
+            UserInfo storage uInfo = userInfo[pid][account];
+            uInfo.rewards = earned(pid, account);
+            uInfo.lastUpdated = block.timestamp;
         }
         _;
     }
 
-    constructor(address _lpAddress, 
+    modifier nonDuplicated(address _lpToken) {
+        require(poolExistence[_lpToken] == false, "nonDuplicated: duplicated");
+        _;
+    }
+
+
+    constructor(address _feeAddress) {
+        feeAddress = _feeAddress;
+    }
+
+    function addPool(address _lpAddress, 
         address _reward, 
-        uint256 _rewardPerBlock, 
+        uint256 _rewardPerWeek, 
         uint256 _unstakingFee,
         uint256 _minStaking,
         uint256 _maxStaking
-        ) {
-        LPAddress = _lpAddress;
-        rewardTokenAddress = _reward;
-        rewardPerWeek = _rewardPerBlock;
-        unstakingFee = _unstakingFee;
-        minStakingAmount = _minStaking;
-        maxStakingAmount = _maxStaking;
-        feeAddress = 0x98B48C1B9654C0bda3cB7A1561b930E754A6641F;
+        ) public onlyOwner nonDuplicated(_lpAddress) {
+        
+        poolExistence[_lpAddress] = true;
+
+        poolInfo.push(PoolInfo({
+            LPAddress: _lpAddress,
+            rewardTokenAddress: _reward,
+            rewardPerWeek: _rewardPerWeek,
+            minStakingAmount: _minStaking,
+            maxStakingAmount: _maxStaking,
+            unstakingFee: _unstakingFee,
+            totalStaked: 0
+        }));
     }
 
-    function setRewardTokenAddress(address _address) external onlyOwner {
+    function setRewardTokenAddress(uint256 _pid, address _address) external onlyOwner {
         require(_address != address(0x0), "invalid address");
-        rewardTokenAddress = _address;
+        poolInfo[_pid].rewardTokenAddress = _address;
 
-        emit RewardAddressUpdated(_address);
+        emit RewardAddressUpdated(_pid, _address);
     }
 
-    function setRewardPerWeek(uint256 _reward) external onlyOwner {
-        rewardPerWeek = _reward;
+    function setRewardPerWeek(uint256 _pid, uint256 _reward) external onlyOwner {
+        poolInfo[_pid].rewardPerWeek = _reward;
 
-        emit RewardPerWeekUpdated(_reward);
+        emit RewardPerWeekUpdated(_pid, _reward);
     }
 
-    function setUnstakingFee(uint256 _fee) external onlyOwner {
+    function setUnstakingFee(uint256 _pid, uint256 _fee) external onlyOwner {
         require(_fee < PERCENTS_DIVIDER, "Unstaking fee is too big");
 
-        unstakingFee = _fee;
+        poolInfo[_pid].unstakingFee = _fee;
     }
     
-    function setMinMaxAmount(uint256 _min, uint256 _max) external onlyOwner {
+    function setMinMaxAmount(uint256 _pid, uint256 _min, uint256 _max) external onlyOwner {
         require(_max > 0 && _max >= _min, "Invalid Max amount");
 
-        minStakingAmount = _min;
-        maxStakingAmount = _max;
+        poolInfo[_pid].minStakingAmount = _min;
+        poolInfo[_pid].maxStakingAmount = _max;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
-        return balances[account];
+    function balanceOf(uint256 _pid, address account) public view returns (uint256) {
+        return userInfo[_pid][account].balance;
     }
 
-    function earned(address account) public view returns (uint256) {
+    function earned(uint256 _pid, address account) public view returns (uint256) {
         uint256 blockTime = block.timestamp;
-        uint256 amount = blockTime.sub(lastUpdated[account]).mul(balanceOf(account)).mul(rewardPerWeek).div(604800).div(PERCENTS_DIVIDER);
-        return rewards[account].add(amount);
+        UserInfo memory uInfo = userInfo[_pid][account];
+        PoolInfo memory pInfo = poolInfo[_pid];
+
+        uint256 amount = blockTime.sub(uInfo.lastUpdated).mul(balanceOf(_pid, account)).mul(pInfo.rewardPerWeek).div(604800).div(PERCENTS_DIVIDER);
+        return uInfo.rewards.add(amount);
     }
 
     
-    function stake(uint256 amount) public updateReward(_msgSender()) nonReentrant whenNotPaused {
-        require(amount.add(balanceOf(_msgSender())) >= minStakingAmount , "Cannot stake less than Min Limitation");
-        require(amount.add(balanceOf(_msgSender())) <= maxStakingAmount, "Cannot stake more than Max Limitation");
+    function stake(uint256 _pid, uint256 amount) public updateReward(_pid, _msgSender()) nonReentrant whenNotPaused {
+        UserInfo storage uInfo = userInfo[_pid][_msgSender()];
+        PoolInfo storage pInfo = poolInfo[_pid];
 
-        require(IERC20(LPAddress).transferFrom(_msgSender(), address(this), amount), "LP token was not successfully transferred to contract");
-        balances[_msgSender()] = balances[_msgSender()].add(amount);
-        totalStaked = totalStaked.add(amount);
-        emit Staked(_msgSender(), amount);
+        require(amount.add(uInfo.balance) >= pInfo.minStakingAmount , "Cannot stake less than Min Limitation");
+        require(amount.add(uInfo.balance) <= pInfo.maxStakingAmount, "Cannot stake more than Max Limitation");
+
+        require(IERC20(pInfo.LPAddress).transferFrom(_msgSender(), address(this), amount), "LP token was not successfully transferred to contract");
+        uInfo.balance = uInfo.balance.add(amount);
+        pInfo.totalStaked = pInfo.totalStaked.add(amount);
+        emit Staked(_pid, _msgSender(), amount);
     }
 
-    function withdraw(uint256 amount) public updateReward(_msgSender()) nonReentrant {
+    function withdraw(uint256 _pid, uint256 amount) public updateReward(_pid, _msgSender()) nonReentrant {
         require(amount > 0, "Cannot withdraw 0");
-        require(amount <= balanceOf(_msgSender()), "Cannot withdraw more than balance");
+        require(amount <= balanceOf(_pid, _msgSender()), "Cannot withdraw more than balance");
 
-        uint256 fee = amount.mul(unstakingFee).div(100);
+        UserInfo storage uInfo = userInfo[_pid][_msgSender()];
+        PoolInfo storage pInfo = poolInfo[_pid];
+        
+        uint256 fee = amount.mul(pInfo.unstakingFee).div(PERCENTS_DIVIDER);
         if(fee > 0) {
-            require(IERC20(LPAddress).transfer(feeAddress, fee), "Transfer unstaking fee failed");
+            require(IERC20(pInfo.LPAddress).transfer(feeAddress, fee), "Transfer unstaking fee failed");
         }
 
-        require(IERC20(LPAddress).transfer(_msgSender(), amount.sub(fee)), "Transfer failed");
-        balances[_msgSender()] = balances[_msgSender()].sub(amount);
-        totalStaked = totalStaked.sub(amount);
-        emit Withdrawn(_msgSender(), amount);
+        require(IERC20(pInfo.LPAddress).transfer(_msgSender(), amount.sub(fee)), "Transfer failed");
+        uInfo.balance = uInfo.balance.sub(amount);
+        pInfo.totalStaked = pInfo.totalStaked.sub(amount);
+        emit Withdrawn(_pid, _msgSender(), amount);
     }
 
-    function exit() external {
-        withdraw(balanceOf(_msgSender()));
-        redeem(rewards[_msgSender()]);
+    function exit(uint256 _pid) external {
+        UserInfo memory uInfo = userInfo[_pid][_msgSender()];
+        withdraw(_pid, uInfo.balance);
+        redeem(_pid, uInfo.rewards);
     }
 
-    function redeem(uint256 amount) public updateReward(_msgSender()) nonReentrant {
+    function redeem(uint256 _pid, uint256 amount) public updateReward(_pid, _msgSender()) nonReentrant {
         require(amount > 0, "Cannot redeem 0");
-        require(amount <= rewards[_msgSender()], "Not enough rewards to redeem");
 
-        ERC20PresetMinterPauser(rewardTokenAddress).mint(_msgSender(), amount);
-        rewards[_msgSender()] = rewards[_msgSender()].sub(amount);
-        emit Redeem(_msgSender(), amount);
+        UserInfo storage uInfo = userInfo[_pid][_msgSender()];
+        PoolInfo storage pInfo = poolInfo[_pid];
+
+        require(amount <= uInfo.rewards, "Not enough rewards to redeem");
+
+        ERC20PresetMinterPauser(pInfo.rewardTokenAddress).mint(_msgSender(), amount);
+        uInfo.rewards = uInfo.rewards.sub(amount);
+        emit Redeem(_pid, _msgSender(), amount);
     }
 }
